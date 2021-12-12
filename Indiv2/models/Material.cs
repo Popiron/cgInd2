300e7dcd120f8e7db19aa8d917fd193a31352dd5
Dmitry Galushko <44999145+Popiron@users.noreply.github.com>
2021-12-12 16:40:12 +0300

    base

---
+++ Indiv2/models/Material.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indiv2.models
+{
+    public class Material
+    {
+        public float reflection;    // коэффициент отражения
+        public float refraction;    // коэффициент преломления
+        public float environment;   // коэффициент преломления среды
+        public float ambient;       // коэффициент принятия фонового освещения
+        public float diffuse;       // коэффициент принятия диффузного освещения
+        public Point3D clr;         // цвет материала
+
+        public Material(float refl, float refr, float amb, float dif, float env = 1)
+        {
+            reflection = refl;
+            refraction = refr;
+            ambient = amb;
+            diffuse = dif;
+            environment = env;
+        }
+
+        public Material(Material m)
+        {
+            reflection = m.reflection;
+            refraction = m.refraction;
+            environment = m.environment;
+            ambient = m.ambient;
+            diffuse = m.diffuse;
+            clr = new Point3D(m.clr);
+        }
+
+        public Material() { }
+    }
+}
